8fa63fe7e328cf13e81891bae4b674ff180bcd2d
Mason Hall <hallmasonschool@gmail.com>
2023-10-07 11:35:08 -0400

    Revert "Save"
    This reverts commit 9f84477a798f7e541d7b1d8ce5ae98b89e1e08c4.

--- OutfitMe/App.js
+++ OutfitMe/App.js
@@ -1,33 +1,23 @@
-import React, { useState } from 'react';
-//import { StyleSheet, Text, TextInput, View, Image, SafeAreaView, TouchableOpacity, Button } from 'react-native';
-//import { supabase } from './components/auth/supabase.js';
-//import SignIn from './components/signIn.js';
-//import SignUp from './components/signUp.js';
-//import Questionaire from './components/questionaire.js';
-import  TranslateText from './components/message.js';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Chatbot from './components/message'; // Import your Chatbot component
 
-const Stack = createStackNavigator();
 
-const App = () => {
+export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        {/* Define a Stack.Screen component for each screen */}
-        {/*<Stack.Screen name="SignIn" component={SignIn} />*/}
-        {/*<Stack.Screen name="SignUp" component={SignUp} />*/}
-        {/*<Stack.Screen name="Questionaire" component={Questionaire} />*/}
-        <Stack.Screen
-  name="Message"
-  component={TranslateText}
-  options={{// Set headerShown to false to hide the header
-  }}
-/>
-
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      {/* Replace the existing content with your Chatbot component */}
+      <Chatbot />
+      <StatusBar style="auto" />
+    </View>
   );
-};
+}
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
